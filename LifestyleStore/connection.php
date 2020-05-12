bbae5953ba58d334e22e6e4e77892882b3e229e4
Abdiwali <46087190+abdiwali145@users.noreply.github.com>
2020-05-12 07:47:38 +0200

    initial commit

---
+++ LifestyleStore/connection.php
@@ -0,0 +1,4 @@
+<?php
+$con=mysqli_connect("localhost","root","Abdiwali12345","db1") or die(mysqli_error($con));
+//$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
+?>
